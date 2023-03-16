b4402db80a022a747a1a25e8dde097d416d4eb91
DeLaKruz <yerayg466@gmail.com>
2023-03-16 04:35:54 +0100

    c) OperacionesTDD.suma(“1,2”) → Devuelve 3
    d) OperacionesTDD.suma(“1,1,2”) → Devuelve 4

--- Tareas-TDD/Tests/OperacionesTDDTest.java
+++ Tareas-TDD/Tests/OperacionesTDDTest.java
@@ -16,4 +16,17 @@ class OperacionesTDDTest {
         int calc = n.sum("1");
         assertEquals(1,calc);
     }
+
+    @Test
+    void SumaTest1_2Devolver3(){
+        OperacionesTDD n = new OperacionesTDD();
+        int calc = n.sum("1,2");
+        assertEquals(3,calc);
+    }
+    @Test
+    void SumaTest1_1_2Devolver4(){
+        OperacionesTDD n = new OperacionesTDD();
+        int calc = n.sum("1,1,2");
+        assertEquals(4,calc);
+    }
 }
